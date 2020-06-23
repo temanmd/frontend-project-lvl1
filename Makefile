bc1ad8e29e60b922265b9a6d73021cf5757bfea2
Artem Belankov <temacxz@gmail.com>
2020-06-23 13:02:17 +0300

    Add brain-calc game

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ install:
 brain-even:
 	node bin/brain-even.js
 
+brain-calc:
+	node bin/brain-calc.js
+
 publish:
 	npm publish --dry-run
 
---
+++ src/brain-calc.js
@@ -0,0 +1,51 @@
+import runGame from './index.js';
+
+const runBrainCalcGame = () => {
+  runGame(generateQuestion, checkAnswer, getCorrectAnswer);
+};
+
+const checkAnswer = (answer, question) => {
+  const integerAnswer = parseInt(answer, 10);
+  switch (question.action) {
+    case '+':
+      return integerAnswer === question.number1 + question.number2;
+    case '-':
+      return integerAnswer === question.number1 - question.number2;
+    case '*':
+      return integerAnswer === question.number1 * question.number2;
+    default:
+      return false;
+  }
+};
+
+const getCorrectAnswer = (question) => {
+  switch (question.action) {
+    case '+':
+      return question.number1 + question.number2;
+    case '-':
+      return question.number1 - question.number2;
+    case '*':
+      return question.number1 * question.number2;
+    default:
+      return null;
+  }
+};
+
+const generateQuestion = () => {
+  const actions = ['+', '-', '*'];
+  const actionIndex = Math.floor(Math.random() * 3);
+  const action = actions[actionIndex];
+
+  const maxNumber = 1000;
+  const number1 = Math.floor(Math.random() * Math.floor(maxNumber));
+  const number2 = Math.floor(Math.random() * Math.floor(maxNumber));
+
+  return {
+    number1,
+    number2,
+    action,
+    text: `${number1} ${action} ${number2}`,
+  };
+};
+
+export default runBrainCalcGame;
