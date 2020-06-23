f26b9faa6d487c8877824d864744b09065e5de37
Artem Belankov <temacxz@gmail.com>
2020-06-23 14:46:02 +0300

    Add brain-progression game

---
+++ src/games/brain-progression.js
@@ -0,0 +1,40 @@
+import runGame from '../index.js';
+
+const runBrainProgressionGame = () => {
+  runGame(getNoticeText, generateQuestion, checkAnswer, getCorrectAnswer);
+};
+
+const getNoticeText = () => 'What number is missing in the progression?';
+
+const checkAnswer = (answer, question) => {
+  const integerAnswer = parseInt(answer, 10);
+  return question.answer === integerAnswer;
+};
+
+const getCorrectAnswer = (question) => question.answer;
+
+const generateQuestion = () => {
+  const stepNumber = Math.floor(Math.random() * 10) + 1;
+  const replaceIndex = Math.floor(Math.random() * 10);
+  const startNumber = Math.floor(Math.random() * Math.floor(100));
+  let counter = startNumber;
+  const numbers = [startNumber];
+
+  for (let i = 0; i < 9; i += 1) {
+    counter += stepNumber;
+    numbers.push(counter);
+  }
+
+  const textNumbers = [...numbers];
+  textNumbers[replaceIndex] = '..';
+  const text = textNumbers.join(' ');
+  const answer = numbers[replaceIndex];
+
+  return {
+    answer,
+    numbers,
+    text,
+  };
+};
+
+export default runBrainProgressionGame;
