0637183c48af60de61275b505a0c39d055a9f013
Artem Belankov <temacxz@gmail.com>
2020-06-23 12:35:57 +0300

    Lint fixes

--- src/brain-even.js
+++ src/brain-even.js
@@ -1,4 +1,3 @@
-import readlineSync from 'readline-sync';
 import runGame from './index.js';
 
 const ANSWER_YES = 'yes';
@@ -20,16 +19,15 @@ const checkAnswer = (answer, question) => {
   return isEven(question.number) === getBooleanAnswer(answer);
 };
 
-const getCorrectAnswer = (question) => {
-  return isEven(question.number) ? ANSWER_YES : ANSWER_NO;
-};
+const getCorrectAnswer = (question) =>
+  (isEven(question.number) ? ANSWER_YES : ANSWER_NO);
 
 const generateQuestion = () => {
   const maxNumber = 1000;
   const number = Math.floor(Math.random() * Math.floor(maxNumber));
 
   return {
-    number: number,
+    number,
     text: number,
   };
 };
